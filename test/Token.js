667ddfceba114ecb47e5cacd1c87a1914acdce56
Shen Yang <shen2.yang@symbio.com>
2024-11-19 17:10:30 +0800

    refactor tests with test fixture

--- test/Token.js
+++ test/Token.js
@@ -8,19 +8,19 @@ describe("Token contract", function () {
     const[owner, addr1, addr2] = await ethers.getSigners();
     const hardhatToken = await ethers.deployContract("Token");
 
-    return [hardhatToken, owner, addr1, addr2];
+    return {hardhatToken, owner, addr1, addr2};
   }
 
   
   it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [hardhatToken, owner, addr1, addr2] = await deployContractFixture();
+    const {hardhatToken, owner} = await loadFixture(deployContractFixture);
 
     const ownerBalance = await hardhatToken.balanceOf(owner.address);
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
   });
 
   it("Should transfer tokens between accounts", async function () {
-    const [hardhatToken, owner, addr1, addr2] = await deployContractFixture();
+    const {hardhatToken, owner, addr1, addr2} = await loadFixture(deployContractFixture);
 
     await hardhatToken.transfer(addr1.address, 50);
     expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
