abe232c5f68a5e1db63ddeebd08ab293ffcbaf54
Shen Yang <shen2.yang@symbio.com>
2024-11-19 16:46:40 +0800

    refactor tests with test fixture

--- test/Token.js
+++ test/Token.js
@@ -1,17 +1,27 @@
 const { expect } = require("chai");
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
 describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner] = await ethers.getSigners();
+  async function deployContractFixture() {
+    const[owner, addr1, addr2] = await ethers.getSigners();
     const hardhatToken = await ethers.deployContract("Token");
+
+    return [hardhatToken, owner, addr1, addr2];
+  }
+
+  
+  it("Deployment should assign the total supply of tokens to the owner", async function () {
+    const [hardhatToken, owner, addr1, addr2] = await deployContractFixture();
 
     const ownerBalance = await hardhatToken.balanceOf(owner.address);
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
   });
 
   it("Should transfer tokens between accounts", async function () {
-    const [owner, addr1, addr2] = await ethers.getSigners();
-    const hardhatToken = await ethers.deployContract("Token");
+    const [hardhatToken, owner, addr1, addr2] = await deployContractFixture();
+
     await hardhatToken.transfer(addr1.address, 50);
     expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
     await hardhatToken.connect(addr1).transfer(addr2.address, 50);
