cf9505d805592b7e86acb2a0cc23d0bfa7a4d926
Shen Yang <shen2.yang@symbio.com>
2024-11-19 16:14:22 +0800

    test transfer tokens

--- test/Token.js
+++ test/Token.js
@@ -8,4 +8,13 @@ describe("Token contract", function () {
     const ownerBalance = await hardhatToken.balanceOf(owner.address);
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
   });
+
+  it("Should transfer tokens between accounts", async function () {
+    const [owner, addr1, addr2] = await ethers.getSigners();
+    const hardhatToken = await ethers.deployContract("Token");
+    await hardhatToken.transfer(addr1.address, 50);
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+    await hardhatToken.connect(addr1).transfer(addr2.address, 50);
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(0);
+  });
 });
