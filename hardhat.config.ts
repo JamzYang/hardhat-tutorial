dc737e6d53a85e9e47e8232d025b4131b5803ad2
Shen Yang <shen2.yang@symbio.com>
2024-11-20 16:13:30 +0800

    load env variables from .env file using dotenv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY
+const SEPOLIA_URL = process.env.SEPOLIA_URL
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [SEPOLIA_PRIVATE_KEY!!],
+    }
+  }
 };
 
 export default config;
